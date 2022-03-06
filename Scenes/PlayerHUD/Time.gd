ad0db4ec8ad25e1b6ac72c64bce255e7c3a0afd6
jmulholland4014 <62855716+jmulholland4014@users.noreply.github.com>
2022-03-06 06:10:10 -0500

    Spacing

--- Scenes/PlayerHUD/Time.gd
+++ Scenes/PlayerHUD/Time.gd
@@ -7,11 +7,9 @@ var str_seconds
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,9 +21,7 @@ func _process(delta):
 		str_seconds = str(seconds)
 	set_text(str(minutes) + ":" + str_seconds)
 	pass
-
-
-
+	
 func _on_Timer_timeout():
 	seconds+=1
 	pass # Replace with function body.
