1f5eb3d2ddfb5fdd6885ab5a4d99b6eae452e0c5
Ivan <goncharukivan.biz@gmail.com>
2022-03-09 13:40:52 -0500

    Implemented a timer

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,7 @@
 extends KinematicBody2D
 class_name Player
 
-
 ### exports ###
-export (bool) var debug_draw = false
-export (bool) var infinite_stamina = false
 
 export (float) var traction_fast = 0.1
 export (float) var traction_slow = 0.7
@@ -17,12 +14,19 @@ export (int) var steering_angle = 15
 export (int) var wheel_base = 70
 export (int) var max_speed_reverse = 50
 
+### Debug ###
+var debug_draw: bool = false
+var infinite_stamina: bool = false
 
 ### Movement ###
 var acceleration := Vector2.ZERO
 var velocity := Vector2.ZERO
 var steer_direction: float
 
+### Time ###
+var _time: float = 0
+var timer_on: bool = false
+var time_passed: String
 
 ### Stamina ###
 export var max_stamina: float = 300
@@ -34,36 +38,55 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	# Movement
 	acceleration = Vector2.ZERO
-	get_input()
-	apply_friction()
-	calculate_steering(delta)
+	_get_input()
+	_apply_friction()
+	_calculate_steering(delta)
 	velocity += acceleration * delta
 	velocity = move_and_slide(velocity)
 	
+	time_passed = _process_time(delta) # clock timer
+	_handle_cheats()
+	_handle_debug()
 	
-	if infinite_stamina:
-		stamina = 1337
-	elif not infinite_stamina and stamina > max_stamina:
-		stamina = max_stamina
-	
-	$LabelContainer.visible = false
-	if debug_draw:
-		$LabelContainer.global_rotation = 0
-		$LabelContainer.visible = true
-		$LabelContainer/SpeedLabel.text = str("%3.1f" % velocity.length())
-		
 
 
-func apply_friction() -> void:
+func _apply_friction() -> void:
 	if velocity.length() < 5:
 		velocity = Vector2.ZERO
 	var friction_force: Vector2 = velocity * friction
 	var drag_force: Vector2 = velocity * velocity.length() * drag
 	acceleration += drag_force + friction_force
 
+"""
+Returns a formatted string for the time
+"""
+func _process_time(delta: float) -> String:
+	_time += delta
+	var milli_secs := fmod(_time, 1) * 1000
+	var secs := fmod(_time, 60)
+	var mins := fmod(_time, 60 * 60) / 60
+	return "%02d : %02d : %03d" % [mins, secs, milli_secs]
+	
 
-func get_input() -> void:
+
+func _handle_cheats() -> void:
+	if infinite_stamina:
+		stamina = 1337
+	elif not infinite_stamina and stamina > max_stamina:
+		stamina = max_stamina
+		
+
+func _handle_debug() -> void:
+	$LabelContainer.visible = false
+	if debug_draw:
+		$LabelContainer.global_rotation = 0
+		$LabelContainer.visible = true
+		$LabelContainer/SpeedLabel.text = str("%3.1f" % velocity.length())
+
+
+func _get_input() -> void:
 	var turn: int = 0
 	if Input.is_action_pressed("steer_right"):
 		turn += 1
@@ -81,11 +104,9 @@ func get_input() -> void:
 	else:
 		if stamina < max_stamina:
 			stamina += stamina_regeneration
-			
 
 
-
-func calculate_steering(delta: float) -> void:
+func _calculate_steering(delta: float) -> void:
 	var rear_wheel: Vector2 = position - transform.x * wheel_base/2.0
 	var front_wheel: Vector2 = position + transform.x * wheel_base/2.0
 	rear_wheel += velocity * delta
@@ -103,6 +124,3 @@ func calculate_steering(delta: float) -> void:
 	elif d < 0:
 		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
 	rotation = new_heading.angle()
-	
-
-
--- PlayerHUD/PlayerHUD.gd
+++ PlayerHUD/PlayerHUD.gd
@@ -6,7 +6,7 @@ var player = null
 func _ready():
 	# if a path to a player exists, set the player to that path
 	if player_path:
-		player = get_node(player_path)
+		player = get_node(player_path) as Player
 
 func _input(_event):
 	pass
@@ -17,3 +17,4 @@ func _process(_delta):
 	if player:
 		$Panel/VBoxContainer/Speedometer/Stamina.text = "%3.1f" % player.stamina
 		$Panel/VBoxContainer/LapCounter/CurrentLap.text = "%d" % Global.get_lap_counter()
+		$Panel/VBoxContainer/Time/CurrentTime.text = player.time_passed
