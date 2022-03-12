9d4584be789fb67ec8cc2e6172bd75a7de921312
Ivan <goncharukivan.biz@gmail.com>
2022-03-12 02:04:04 -0500

    semi working start screen

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -1,6 +1,14 @@
 extends Control
 
+func _ready():
+	$Vbox/Start.grab_focus()
 
-
-func _on_Start_pressed():
+func _on_Start_pressed() -> void:
 	get_tree().change_scene("res://Levels/Level.tscn")
+
+func _on_Options_pressed():
+	var options_menu = load("res://UI/OptionsMenu.tscn").instance()
+	get_tree().current_scene.add_child(options_menu)
+
+func _on_Quit_pressed():
+	get_tree().quit()
