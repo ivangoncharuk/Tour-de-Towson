63074fd3fa8263d6795b2e2fe9a2bb97dc1ef6a8
ivangoncharuk <goncharukivan.biz@gmail.com>
2022-03-21 04:22:58 -0400

    LevelManager refactored code, added documentation

--- Levels/LevelManager.gd
+++ Levels/LevelManager.gd
@@ -11,36 +11,38 @@ onready var is_player_hud: bool = player_hud.visible
 onready var is_control_panel: bool = control_panel.visible
 
 
-func _unhandled_input(event) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
+		# self. refers to the setter -> setget set_is_paused
 		self.is_paused = !is_paused
-		if not is_paused:
-			player_hud.visible = is_player_hud
-			control_panel.visible = is_control_panel
-		else:
-			control_panel.hide()
-			player_hud.hide()
+	
+	# these input events are blocked by paused state
 	elif not is_paused:
-		# cannot toggle control_panel & player_hud when is_paused	
 		if event.is_action_pressed("toggle_player_hud"):
 			player_hud.visible = !player_hud.visible
 			is_player_hud = player_hud.visible
+		
 		elif event.is_action_pressed("toggle_control_panel"):
 			control_panel.visible = !control_panel.visible
 			is_control_panel = control_panel.visible
-
 
 func set_is_paused(value: bool) -> void:
 	# pauses the whole tree
 	get_tree().paused = value
 	is_paused = value
 	pause_menu.visible = is_paused
-
 	
+	# ui elements that were toggled by a key, will go back to their state
+	if not is_paused:
+		player_hud.visible = is_player_hud
+		control_panel.visible = is_control_panel
+	# hiding elements when game is paused
+	else:
+		control_panel.hide()
+		player_hud.hide()
 
 func _on_Resume_pressed():
 	self.is_paused = false
-
 
 func _on_Quit_pressed():
 	get_tree().quit()
