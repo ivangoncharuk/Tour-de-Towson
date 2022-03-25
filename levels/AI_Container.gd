87b8386ff29ed1c6d629767a8687700449a1c9f5
jmulholland4014 <62855716+jmulholland4014@users.noreply.github.com>
2022-03-25 12:49:37 -0400

    AI Completed

--- levels/AI_Container.gd
+++ levels/AI_Container.gd
@@ -7,3 +7,12 @@ func get_path_direction(pos):
 	path_follow.offset = offset
 	return path_follow.transform.x
 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
